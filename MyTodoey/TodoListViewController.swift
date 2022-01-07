da29b2a684e9a3aa4c13e1f289b44507927e1e0f
Keith Steffen <ksteffen4@gmail.com>
2022-01-06 21:24:17 -0600

    Ready to start adding navigation

--- MyTodoey/TodoListViewController.swift
+++ MyTodoey/TodoListViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("in \(#function)")
     }
 
 
