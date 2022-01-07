29341857adb4057ee7e9adcb4b4eee48686456c2
Keith Steffen <ksteffen4@gmail.com>
2022-01-07 03:24:15 -0600

    Added UINavigationViewControll and displayed dummy todo list.

--- MyTodoey/TodoListViewController.swift
+++ MyTodoey/TodoListViewController.swift
@@ -9,12 +9,22 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("in \(#function)")
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
 }
 
